25e9f27fb3af82c9dde49d3f6321655dc7ef0f24
Anicet Kesraoui <anicetkesraoui@gmail.com>
2018-02-05 16:53:47 +0100

    style animation flo

---
+++ public/assets/js/titleAnimation/titleMdpAnimation.js
@@ -0,0 +1,28 @@
+// function([string1, string2],target)    
+consoleText(["Réinisialisation du mot de passe"], 'text');
+
+function consoleText(words, id) {
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        letterCount += x;
+        waiting = false;
+      }, 5500)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 60)
+}
